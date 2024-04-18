57b2d3d76a4e5dc6def77fa985afb0d590d457c9
Andrew Chen <andrew@jaanuu.com>
2024-04-17 23:51:14 -0700

    fix

--- audio_offset_finder_v2.py
+++ audio_offset_finder_v2.py
@@ -2,6 +2,7 @@ import argparse
 from collections import deque
 import copy
 import datetime
+import os
 import pdb
 import time
 import librosa
@@ -128,7 +129,7 @@ def mfcc_method(clip,audio,sr):
     # method_count = method_count + 1
     # return match_times
 
-def correlation_method(clip,audio,sr,index,seconds_per_chunk):
+def correlation_method(clip,audio,sr,index,seconds_per_chunk,clip_name):
     global method_count
     threshold = 0.8  # Threshold for distinguishing peaks, need to be smaller for larger clips
     # Cross-correlate and normalize correlation
@@ -140,14 +141,15 @@ def correlation_method(clip,audio,sr,index,seconds_per_chunk):
     #print(len(audio))
     correlation=correlation[:-len(clip)]
 
-    # #Optional: plot the correlation graph to visualize
-    # plt.figure(figsize=(10, 4))
-    # plt.plot(correlation)
-    # plt.title('Cross-correlation between the audio clip and full track')
-    # plt.xlabel('Lag')
-    # plt.ylabel('Correlation coefficient')
-    # plt.savefig(f'./tmp/cross_correlation_{index}_{str(datetime.timedelta(seconds=index*seconds_per_chunk))}.png')
-    # plt.close()
+    os.makedirs("./tmp/graph", exist_ok=True)
+    #Optional: plot the correlation graph to visualize
+    plt.figure(figsize=(10, 4))
+    plt.plot(correlation)
+    plt.title('Cross-correlation between the audio clip and full track')
+    plt.xlabel('Lag')
+    plt.ylabel('Correlation coefficient')
+    plt.savefig(f'./tmp/graph/cross_correlation_{clip_name}_{index}_{str(datetime.timedelta(seconds=index*seconds_per_chunk))}.png')
+    plt.close()
 
     peak_max = np.max(correlation)
     index_max = np.argmax(correlation)
@@ -173,7 +175,7 @@ def correlation_method(clip,audio,sr,index,seconds_per_chunk):
 # sliding_window: for previous_chunk in seconds from end
 # index: for debugging by saving a file for audio_section
 # seconds_per_chunk: default seconds_per_chunk
-def process_chunk(chunk, clip, sr, previous_chunk,sliding_window,index,seconds_per_chunk,method="correlation"):
+def process_chunk(chunk, clip, sr, previous_chunk,sliding_window,index,seconds_per_chunk,clip_name,method="correlation"):
     clip_length = len(clip)
     new_seconds = len(chunk)/sr
     # Concatenate previous chunk for continuity in processing
@@ -208,23 +210,31 @@ def process_chunk(chunk, clip, sr, previous_chunk,sliding_window,index,seconds_p
 
     # peak normalize audio to -1 dB
     #clip = pyln.normalize.peak(clip, -1.0)
-
+    #sf.write(f"./tmp/clipbefore.wav", copy.deepcopy(clip), sr)
 
     clip = clip / np.max(np.abs(clip))
-    if(clip_length / sr > 1): # Normalize clip more than 1 second
-        # measure the loudness first 
+    clip_second = clip_length / sr
+
+    #sf.write(f"./tmp/clipbefore2.wav", copy.deepcopy(clip), sr)
+
+    # measure the loudness first 
+    
+    if(clip_second < 0.5):
+        meter = pyln.Meter(sr, block_size=clip_second)
+    else:
         meter = pyln.Meter(sr) # create BS.1770 meter
-        loudness = meter.integrated_loudness(clip)
+    loudness = meter.integrated_loudness(clip)
 
-        # loudness normalize audio to -12 dB LUFS
-        clip = pyln.normalize.loudness(clip, loudness, -12.0)
-
+    # loudness normalize audio to -12 dB LUFS
+    clip = pyln.normalize.loudness(clip, loudness, -12.0)
+    #sf.write(f"./tmp/clip.wav", copy.deepcopy(clip), sr)
     audio_section = np.concatenate((audio_section,clip))
 
-    sf.write(f"./tmp/audio_section{index}_{str(datetime.timedelta(seconds=index*seconds_per_chunk))}.wav", copy.deepcopy(audio_section), sr)
+    os.makedirs("./tmp/audio", exist_ok=True)
+    sf.write(f"./tmp/audio/section_{clip_name}_{index}_{str(datetime.timedelta(seconds=index*seconds_per_chunk))}.wav", copy.deepcopy(audio_section), sr)
 
     if method == "correlation":
-        peak_times = correlation_method(clip, audio=audio_section, sr=sr,index=index,seconds_per_chunk=seconds_per_chunk)
+        peak_times = correlation_method(clip, audio=audio_section, sr=sr,index=index,seconds_per_chunk=seconds_per_chunk, clip_name=clip_name)
     elif method == "mfcc":
         peak_times = mfcc_method(clip, audio=audio_section, sr=sr)
     elif method == "chroma_method":
@@ -234,7 +244,7 @@ def process_chunk(chunk, clip, sr, previous_chunk,sliding_window,index,seconds_p
 
     peak_times2 = []
     for t in peak_times:
-        if t >= 0 and t >= (len(audio_section)-len(clip) - 1) / sr:
+        if t >= 0 and t >= (len(audio_section)-clip_length - 1) / sr:
             #skip the placeholder clip at the end
             #print("skipping placeholder peak",t)
             continue
@@ -348,11 +358,12 @@ def find_clip_in_audio_in_chunks(clip_path, full_audio_path, method="correlation
         #print("chunk....")
         #print(len(chunk))
         #exit(1)
-        # Process audio data with Librosa (e.g., feature extraction)
-        # ... your Librosa processing here ...
+        clip_name,_ = os.path.splitext(os.path.basename(clip_path))
         peak_times = process_chunk(chunk=chunk, clip=clip, sr=target_sample_rate, 
                                                 previous_chunk=previous_chunk,
-                                                sliding_window=sliding_window,index=i,
+                                                sliding_window=sliding_window,
+                                                index=i,
+                                                clip_name=clip_name,
                                                 seconds_per_chunk=seconds_per_chunk, method=method)
         if len(peak_times):
             peak_times_from_beginning = [time + (i*seconds_per_chunk) for time in peak_times]
