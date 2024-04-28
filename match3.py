ad0202b60ec27d39e497e2e8c7f3971b0ac0d5fe
Andrew Chen <andrew@jaanuu.com>
2024-04-27 22:02:09 -0700

    fix clean up

--- match3.py
+++ match3.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 import ffmpeg
 import librosa
 import soundfile as sf
-from audio_offset_finder_v2 import find_clip_in_audio_in_chunks, DEFAULT_METHOD
+from audio_offset_finder_v2 import find_clip_in_audio_in_chunks, DEFAULT_METHOD, cleanup_peak_times
 from andrew_utils import seconds_to_time
 
 def main():
@@ -28,8 +28,10 @@ def main():
     # Find clip occurrences in the full audio
     peak_times = find_clip_in_audio_in_chunks(args.pattern_file, args.audio_file, method=args.match_method)
     print(peak_times)
+    peak_times_clean = cleanup_peak_times(peak_times)
+    print(peak_times_clean)
 
-    for offset in peak_times:
+    for offset in peak_times_clean:
         print(f"Clip occurs at the following times (in seconds): {seconds_to_time(seconds=offset,include_decimals=False)}" )
     #    #print(f"Offset: {offset}s" )
     
