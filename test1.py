f9906f85e19e5698055ece792e2f19f6b1529131
Andrew Chen <andrew@jaanuu.com>
2024-04-29 07:14:09 -0700

    test

--- test1.py
+++ test1.py
@@ -4,14 +4,14 @@ import librosa
 import numpy as np
 
 from scrape import extract_prefix
-
+from sklearn.metrics.pairwise import cosine_similarity
 
 print(extract_prefix("testagain2022041"))  # None
 print(extract_prefix("testagain20220414"))  # returns ('testagain', '20220414')
 print(extract_prefix("testagain220220414"))  # returns ('testagain2', '20220414')
 print(extract_prefix("testagain220220414hahanada"))  # returns ('testagain2', '20220414')
 
-hop_length = 1024
+hop_length = 512
 y_ref, sr = librosa.load(librosa.ex('pistachio'))
 y_comp, sr = librosa.load(librosa.ex('pistachio'), offset=10)
 #chroma_ref = librosa.feature.chroma_cqt(y=y_ref, sr=sr, hop_length=hop_length)
@@ -22,13 +22,19 @@ y_comp, sr = librosa.load(librosa.ex('pistachio'), offset=10)
 #xsim = librosa.segment.cross_similarity(x_comp, x_ref)
 
 # Extract features from the audio clip and the pattern
-audio_features = librosa.feature.mfcc(y=y_comp, sr=sr)
-pattern_features = librosa.feature.mfcc(y=y_ref, sr=sr)
+audio_features = librosa.feature.mfcc(y=y_comp, sr=sr,hop_length=hop_length)
+pattern_features = librosa.feature.mfcc(y=y_ref, sr=sr,hop_length=hop_length)
 
 # Compute the similarity matrix between the audio features and the pattern features
-similarity_matrix = librosa.segment.cross_similarity(audio_features, pattern_features)
+#similarity_matrix = librosa.segment.cross_similarity(audio_features, pattern_features)
 
-print(similarity_matrix,file=sys.stderr)
+#print(similarity_matrix,file=sys.stderr)
 
-indices = np.argmax(similarity_matrix, axis=1)
+#indices = np.argmax(similarity_matrix, axis=1)
 #print(sorted(indices*hop_length/sr))
+
+similarity = cosine_similarity(audio_features.T, pattern_features.T)
+#print("Cosine Similarity: ", similarity)
+
+indices = np.argmax(similarity, axis=1)
+print(sorted(indices*hop_length/sr))
