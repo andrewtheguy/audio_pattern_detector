6e4bd4ae7fee0aee0a0eb0c99d1e7f9318aa6e85
Andrew Chen <andrew@jaanuu.com>
2024-04-30 23:33:17 -0700

    working dfw test

--- test2.py
+++ test2.py
@@ -1,18 +1,28 @@
 import librosa
 from dtw import dtw
 
-pattern, _ = librosa.load(librosa.ex('pistachio'))
-y, sr = librosa.load(librosa.ex('pistachio'), offset=10)
+import librosa
+import matplotlib.pyplot as plt
+from dtw import dtw
 
+#Loading audio files
+y1, sr1 = librosa.load(librosa.ex('pistachio'))
+y2, sr2 = librosa.load(librosa.ex('pistachio'), offset=10)
 
-# Calculate onset strength for both the pattern and the main audio file
-onset_env_y = librosa.onset.onset_strength(y=y, sr=sr)
-onset_env_pattern = librosa.onset.onset_strength(y=pattern, sr=sr)
+#Showing multiple plots using subplot
+plt.subplot(1, 2, 1)
+mfcc1 = librosa.feature.mfcc(y=y1,sr=sr1)   #Computing MFCC values
+librosa.display.specshow(mfcc1)
 
-# Use Dynamic Time Warping to align the two onset strength sequences
-alignment = dtw(onset_env_y, onset_env_pattern, keep_internals=True)
+plt.subplot(1, 2, 2)
+mfcc2 = librosa.feature.mfcc(y=y2, sr=sr2)
+librosa.display.specshow(mfcc2)
 
-# Print the normalized index of pattern in y
-#print('Normalized index: ', alignment.normalizedIndex[0])
-## Display the warping curve, i.e. the alignment curve
-plot=alignment.plot(type="threeway")
+alignment = dtw(mfcc1.T, mfcc2.T,keep_internals=True)
+dist=alignment.normalizedDistance
+print("The normalized distance between the two : ",dist)   # 0 for similar audios
+
+plt.imshow(alignment.costMatrix, origin='lower', cmap=plt.get_cmap('gray'), interpolation='nearest')
+plt.plot(alignment.index1, alignment.index2, 'w')   #creating plot for DTW
+
+plt.show()  #To display the plots graphically
