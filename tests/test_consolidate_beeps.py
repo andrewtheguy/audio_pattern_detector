28099e27df660a4a3864a6bbeda771890431bb06
Andrew Chen <andrew@jaanuu.com>
2024-05-05 19:26:04 -0700

    wip

--- tests/test_consolidate_beeps.py
+++ tests/test_consolidate_beeps.py
@@ -4,71 +4,71 @@ import numpy as np
 from process_timestamps import consolidate_beeps
 
 class TestConsolidateBeeps(unittest.TestCase):
-    def do_test(self,news_report):
-        return consolidate_beeps(news_report)
+    def do_test(self,news_reports):
+        return consolidate_beeps(news_reports)
     
     def test_zero_everything(self):
-        result = self.do_test(news_report=[])
+        result = self.do_test(news_reports=[])
         np.testing.assert_array_equal(result,[])
         
     def test_only_one(self):
-        result = self.do_test(news_report=[10])
+        result = self.do_test(news_reports=[10])
         np.testing.assert_array_equal(result,[10])
 
     def test_no_repeat(self):
-        result = self.do_test(news_report=[10,30,50,70])
+        result = self.do_test(news_reports=[10,30,50,70])
         np.testing.assert_array_equal(result,[10,30,50,70])
         
     def test_repeat_beginning(self):
-        result = self.do_test(news_report=[10,11,12,13,30,50,70])
+        result = self.do_test(news_reports=[10,11,12,13,30,50,70])
         np.testing.assert_array_equal(result,[10,30,50,70])
         
     def test_repeat_middle(self):
-        result = self.do_test(news_report=[10,30,31,32,33,50,70])
+        result = self.do_test(news_reports=[10,30,31,32,33,50,70])
         np.testing.assert_array_equal(result,[10,30,50,70])
 
     def test_repeat_end(self):
-        result = self.do_test(news_report=[10,30,50,70,71,72,73,74])
+        result = self.do_test(news_reports=[10,30,50,70,71,72,73,74])
         np.testing.assert_array_equal(result,[10,30,50,70])
         
     def test_consecutive_good_ones_with_repeat(self):
-        result = self.do_test(news_report=[10,30,50,51,52,53,70,71,72,73,74])
+        result = self.do_test(news_reports=[10,30,50,51,52,53,70,71,72,73,74])
         np.testing.assert_array_equal(result,[10,30,50,70])
         
     def test_3_repeats(self):
-        result = self.do_test(news_report=[11,12,13,14,30,50,70])
+        result = self.do_test(news_reports=[11,12,13,14,30,50,70])
         np.testing.assert_array_equal(result,[11,30,50,70])
 
     def test_4_repeats(self):
-        result = self.do_test(news_report=[11,12,13,14,15,30,50,70])
+        result = self.do_test(news_reports=[11,12,13,14,15,30,50,70])
         np.testing.assert_array_equal(result,[11,30,50,70])
         
     def test_4_repeats_end(self):
-        result = self.do_test(news_report=[11,30,50,70,71,72,73,74])
+        result = self.do_test(news_reports=[11,30,50,70,71,72,73,74])
         np.testing.assert_array_equal(result,[11,30,50,70])
 
     def test_5_repeats(self):
-        result = self.do_test(news_report=[11,12,13,14,15,16,30,50,70])
+        result = self.do_test(news_reports=[11,12,13,14,15,16,30,50,70])
         np.testing.assert_array_equal(result,[11,30,50,70])
         
     def test_5_repeats_middle(self):
-        result = self.do_test(news_report=[7,11,12,13,14,15,16,30,50,70])
+        result = self.do_test(news_reports=[7,11,12,13,14,15,16,30,50,70])
         np.testing.assert_array_equal(result,[7,11,30,50,70])
         
     def test_5_repeats_end(self):
-        result = self.do_test(news_report=[11,30,50,70,71,72,73,74,75])
+        result = self.do_test(news_reports=[11,30,50,70,71,72,73,74,75])
         np.testing.assert_array_equal(result,[11,30,50,70])
         
     def test_6_repeats(self):
-        result = self.do_test(news_report=[11,12,13,14,15,16,17,30,50,70])
+        result = self.do_test(news_reports=[11,12,13,14,15,16,17,30,50,70])
         np.testing.assert_array_equal(result,[11,17,30,50,70])
         
     def test_6_repeats_middle(self):
-        result = self.do_test(news_report=[7,11,12,13,14,15,16,17,30,50,70])
+        result = self.do_test(news_reports=[7,11,12,13,14,15,16,17,30,50,70])
         np.testing.assert_array_equal(result,[7,11,17,30,50,70])
         
     def test_6_repeats_end(self):
-        result = self.do_test(news_report=[11,30,50,70,71,72,73,74,75,76])
+        result = self.do_test(news_reports=[11,30,50,70,71,72,73,74,75,76])
         np.testing.assert_array_equal(result,[11,30,50,70,76])
         
 
---
+++ tests/test_consolidate_intros.py
@@ -0,0 +1,133 @@
+import unittest
+import numpy as np
+
+from process_timestamps import consolidate_intros
+
+class TestConsolidateIntros(unittest.TestCase):
+    def do_test(self,intros,news_reports):
+        return consolidate_intros(intros,news_reports)
+    
+    def test_zero_everything(self):
+        result = self.do_test(intros=[],
+                              news_reports=[])
+        np.testing.assert_array_equal(result,
+                                      [])
+        
+    def test_news_reports_only(self):
+        result = self.do_test(intros=[],
+                              news_reports=[3,4])
+        np.testing.assert_array_equal(result,
+                                      [])
+        result = self.do_test(intros=[],
+                              news_reports=[3,4])
+        np.testing.assert_array_equal(result,
+                                      [])
+        
+    def test_intros_only(self):
+        result = self.do_test(intros=[3],
+                              news_reports=[])
+        np.testing.assert_array_equal(result,
+                                      [3])
+        
+        result = self.do_test(intros=[3,4],
+                              news_reports=[])
+        np.testing.assert_array_equal(result,
+                                      [3,4])
+        
+    def test_news_report_in_between_intros(self):
+        result = self.do_test(intros=[20,70],
+                              news_reports=[50])
+        np.testing.assert_array_equal(result,
+                                      [20,70])
+        
+        result = self.do_test(intros=[20,50],
+                              news_reports=[50])
+        np.testing.assert_array_equal(result,
+                                      [20,50])
+        
+        result = self.do_test(intros=   [20,50,70],
+                              news_reports=[50])
+        np.testing.assert_array_equal(result,
+                                      [20,50])
+        
+    def test_no_extras_only(self):
+        result = self.do_test(intros=      [100,800],
+                              news_reports=[300,1200,5000])
+        np.testing.assert_array_equal(result,
+                                      [100,800])
+        
+        result = self.do_test(intros=      [300,800,1800],
+                              news_reports=[400,1500,2800])
+        np.testing.assert_array_equal(result,
+                                      [300,800,1800])
+        
+        result = self.do_test(intros=      [300,800,1800,3200],
+                              news_reports=[400,1500,2800])
+        np.testing.assert_array_equal(result,
+                                      [300,800,1800,3200])
+        
+        result = self.do_test(intros=      [400],
+                              news_reports=[800])
+        np.testing.assert_array_equal(result,
+                                      [400])
+        
+        result = self.do_test(intros=      [800],
+                              news_reports=[400])
+        np.testing.assert_array_equal(result,
+                                      [800])
+        
+    def test_extras_news_reports_only(self):      
+        result = self.do_test(intros=      [800],
+                              news_reports=[400,1500,2800])
+        np.testing.assert_array_equal(result,
+                                      [800])
+        
+        result = self.do_test(intros=          [1600,2900],
+                              news_reports=[400,1500,2800,3200,3400])
+        np.testing.assert_array_equal(result,
+                                      [1600,2900])
+        
+
+        
+    def test_extras_no_overlapping(self):
+        result = self.do_test(intros=      [100,200,800,810,820],
+                              news_reports=[    300,        1200,5000])
+        np.testing.assert_array_equal(result,
+                                      [100,800])
+        
+        result = self.do_test(intros=      [300,800,1800,     2801,2802],
+                              news_reports=[400,1500,    2800])
+        np.testing.assert_array_equal(result,
+                                      [300,800,1800,2801])
+        
+    def test_extras_no_overlapping_repeating(self):
+        result = self.do_test(intros=      [100,200,200,800,810,810,820],
+                              news_reports=[        300,            1200,5000])
+        np.testing.assert_array_equal(result,
+                                      [100,800])
+        
+        
+    def test_extras_overlapping(self):
+        result = self.do_test(intros=      [100,300,800,810,1200],
+                              news_reports=[300,       1200,5000])
+        np.testing.assert_array_equal(result,
+                                      [100,300,1200])
+        
+        result = self.do_test(intros=      [300,800,1800,     2800],
+                              news_reports=[400,1500,    2800])
+        np.testing.assert_array_equal(result,
+                                      [300,800,1800,2800])
+        
+        result = self.do_test(intros=      [300,800,1800,     2800,2900],
+                              news_reports=[400,1500,    2800])
+        np.testing.assert_array_equal(result,
+                                      [300,800,1800,2800])
+        
+    def test_extras_overlapping_repeating(self):
+        result = self.do_test(intros=      [100,200,200,300,300,800,810,810,820],
+                              news_reports=[                300,            1200,5000])
+        np.testing.assert_array_equal(result,
+                                      [100,300])
+        
+if __name__ == '__main__':
+    unittest.main()
