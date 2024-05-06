c7af486deb0e6155fcc6ec7bb93081b05f7ab34b
Andrew Chen <andrew@jaanuu.com>
2024-05-06 14:09:10 -0700

    better

--- tests/test_consolidate_beeps.py
+++ tests/test_consolidate_beeps.py
@@ -5,7 +5,7 @@ from process_timestamps import consolidate_beeps
 
 class TestConsolidateBeeps(unittest.TestCase):
     def do_test(self,news_reports):
-        return consolidate_beeps(list(dict.fromkeys(news_reports)))
+        return consolidate_beeps(list(dict.fromkeys(news_reports)),max_seconds=6)
     
     def test_zero_everything(self):
         result = self.do_test(news_reports=[])
