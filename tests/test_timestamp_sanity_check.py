c8eb1875506efb5be95b907c7756c571649474d6
Andrew Chen <andrew@jaanuu.com>
2024-05-08 12:50:15 -0700

    test

--- tests/test_timestamp_sanity_check.py
+++ tests/test_timestamp_sanity_check.py
@@ -1,16 +1,16 @@
 import unittest
 import numpy as np
 
-from process_timestamps import timestamp_sanity_check_rthk
+from process_timestamps import timestamp_sanity_check
 from utils import minutes_to_seconds
 from time_sequence_error import TimeSequenceError
 
-class TestDurationAndGaps(unittest.TestCase):
+class TestTimestampSanityCheck(unittest.TestCase):
     def setUp(self):
         self.total_time_1=minutes_to_seconds(120)
 
     def check(self,result,allow_first_short=False):
-        return timestamp_sanity_check_rthk(result,allow_first_short=allow_first_short,total_time=self.total_time_1)
+        return timestamp_sanity_check(result,total_time=self.total_time_1)
 
 
     def test_empty_array(self):
@@ -49,36 +49,10 @@ class TestDurationAndGaps(unittest.TestCase):
 
     def test_valid(self):
         try:
-            result = self.check([[1,minutes_to_seconds(18)]])
+            result = self.check([[1,minutes_to_seconds(18)],[minutes_to_seconds(19),minutes_to_seconds(21)]])
         except Exception as e:  
             self.fail(f"myFunc() raised {type(e)}: {e} unexpectedly!")
 
-    def test_too_short(self):
-        with self.assertRaises(TimeSequenceError):
-            result = self.check([[1,minutes_to_seconds(14)]])
-
-    def test_allow_first_short(self):
-        #with self.assertRaises(TimeSequenceError):
-        result = self.check([[1,minutes_to_seconds(6)]],allow_first_short=True)
-        result = self.check([
-                [4,minutes_to_seconds(9)],
-                [minutes_to_seconds(12), minutes_to_seconds(30)],
-            ],allow_first_short=True)
-        
-    def test_allow_first_short_only_close_enough_to_beginning(self):
-        with self.assertRaises(TimeSequenceError):
-            result = self.check([
-                [1,minutes_to_seconds(13)],
-                [minutes_to_seconds(14), minutes_to_seconds(18)],
-            ],
-            allow_first_short=True)
-
-
-    def test_gap_normal(self):
-        try:
-            result = self.check([[1,minutes_to_seconds(20)],[minutes_to_seconds(25),minutes_to_seconds(50)]])
-        except TimeSequenceError as e:  
-            self.fail(f"myFunc() raised {type(e)}: {e} unexpectedly!")
 
     def test_allow_continuous(self):
         try:
@@ -86,13 +60,9 @@ class TestDurationAndGaps(unittest.TestCase):
         except Exception as e:  
             self.fail(f"myFunc() raised {type(e)}: {e} unexpectedly!")
 
-    def test_gap_too_large(self):
-        with self.assertRaises(TimeSequenceError):
-            result = self.check([[1,minutes_to_seconds(20)],[minutes_to_seconds(50),minutes_to_seconds(60)]])
-
-    def test_disallow_flip_over(self):
+    def test_disallow_out_of_order(self):
         with self.assertRaises(ValueError):
-            result = self.check([[1,minutes_to_seconds(20)],[minutes_to_seconds(19),minutes_to_seconds(60)]])
+            result = self.check([[minutes_to_seconds(20),minutes_to_seconds(50)],[1,minutes_to_seconds(19)]])
 
     def test_disallow_negative(self):
         with self.assertRaises(ValueError):
