eca15e44e835547c8a1298fa79ded10be5cf813a
Andrew Chen <andrew@jaanuu.com>
2024-05-12 21:52:15 -0700

    update

--- schedule.py
+++ schedule.py
@@ -8,5 +8,6 @@ logging.getLogger('apscheduler').setLevel(logging.INFO)
 if __name__ == '__main__':
     scheduler = BlockingScheduler()
     scheduler.add_job(download_and_scrape, 'interval', hours=1)
+    # add a job to run immediately
     scheduler.add_job(download_and_scrape)
     scheduler.start()
