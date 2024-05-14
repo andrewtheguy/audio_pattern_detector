c2942f9f8d3d2625263b8a789f333d88dfd77767
Andrew Chen <andrew@jaanuu.com>
2024-05-14 09:47:30 -0700

    move

--- utils.py
+++ utils.py
@@ -39,12 +39,12 @@ def hours_to_seconds(hours):
     return minutes_to_seconds(hours*60)
 
 
-array1 = [1, 2, 3, 4, 5]  # Unique and sorted
-array2 = [1, 2, 2, 4, 5]  # Not unique
-array3 = [1, 4, 2, 3, 5]  # Unique but not sorted
-array4 = [1, 2, 4, 2, 5]  # Not unique
-
 if __name__ == '__main__':
+  array1 = [1, 2, 3, 4, 5]  # Unique and sorted
+  array2 = [1, 2, 2, 4, 5]  # Not unique
+  array3 = [1, 4, 2, 3, 5]  # Unique but not sorted
+  array4 = [1, 2, 4, 2, 5]  # Not unique
+
   assert(is_unique_and_sorted(array1)==True)  # Output: True
   assert(is_unique_and_sorted(array2)==False)  # Output: False
   assert(is_unique_and_sorted(array3)==False)  # Output: False
