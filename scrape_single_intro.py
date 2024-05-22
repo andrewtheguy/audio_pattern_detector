7538249bd2bb8550deec837434c27accbe91888a
Andrew Chen <andrew@jaanuu.com>
2024-05-21 17:27:16 -0700

    fix

--- scrape_single_intro.py
+++ scrape_single_intro.py
@@ -157,7 +157,7 @@ def scrape_single_intro(input_file,stream_name,recorded):
         splits=split_audio_by_time_sequences(input_file,total_time,pair,tmpdir)
         concatenate_audio(splits, output_file_trimmed,tmpdir,channel_name="am1430",total_time=total_time)
         upload_path_trimmed = f"/am1430/trimmed/{dirname}/{filename_trimmed}"
-        upload_file(output_file_trimmed,upload_path_trimmed,skip_if_exists=True)
+        upload_file_with_sftp(output_file_trimmed,upload_path_trimmed,skip_if_exists=True)
         
     return output_dir_trimmed,output_file_trimmed
 
--- test_upload.py
+++ test_upload.py
@@ -1,2 +1,2 @@
 from file_upload.upload_utils2 import upload_file
-upload_file("test_upload.py","/test_upload.py")
+upload_file_with_sftp("test_upload.py","/test_upload.py")
