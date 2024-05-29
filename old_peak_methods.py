64114e9d3614c9ef1e14fff92ce9e6937b71c2c8
Andrew Chen <andrew@jaanuu.com>
2024-05-29 13:00:50 -0700

    fix

--- old_peak_methods.py
+++ old_peak_methods.py
@@ -1,5 +1,77 @@
 import logging
 
+def verify_peak(sr,max_index,correlation,audio_section,section_ts,clip_name,index,debug_mode):
+    factor = sr/10
+
+    print(max_index)
+
+    #wlen = max(int(sr/2), int(clip_length))
+    padding = sr*4
+
+    beg = max(int(max_index-padding), 0)
+    end = min(len(audio_section),int(max_index+padding))
+    #print("chafa")
+    #print(beg,end)
+    #exit(1)
+
+    correlation = correlation[beg:end]
+    correlation = downsample(correlation,int(factor))
+
+    if debug_mode:
+        graph_dir = f"./tmp/graph/resampled/{clip_name}"
+        os.makedirs(graph_dir, exist_ok=True)
+
+        #Optional: plot the correlation graph to visualize
+        plt.figure(figsize=(10, 4))
+        # if clip_name == "漫談法律intro" and index == 10:
+        #     plt.plot(correlation[454000:454100])
+        # elif clip_name == "漫談法律intro" and index == 11:
+        #     plt.plot(correlation[50000:70000])
+        # elif clip_name == "日落大道smallinterlude" and index == 13:
+        #     plt.plot(correlation[244100:244700])
+        # elif clip_name == "日落大道smallinterlude" and index == 14:
+        #     plt.plot(correlation[28300:28900])
+        # elif clip_name == "繼續有心人intro" and index == 10:
+        #     plt.plot(correlation[440900:441000])
+        # else:
+        #     plt.plot(correlation)
+        plt.plot(correlation)
+
+        plt.title('Cross-correlation between the audio clip and full track before slicing')
+        plt.xlabel('Lag')
+        plt.ylabel('Correlation coefficient')
+        plt.savefig(
+            f'{graph_dir}/{clip_name}_{index}_{section_ts}.png')
+        plt.close()
+
+    peaks, properties = find_peaks(correlation, width=0, threshold=0, wlen=10, height=0, prominence=0.2, rel_height=1)
+    if debug_mode:
+        peak_dir = f"./tmp/peaks/resampled/{clip_name}"
+        os.makedirs(peak_dir, exist_ok=True)
+        peaks_test=[]
+        for i,item in enumerate(peaks):
+            #plot_test_x=np.append(plot_test_x, index)
+            #plot_test_y=np.append(plot_test_y, item)
+            peaks_test.append([{"index":int(item),"second":item/sr,
+                                "height":properties["peak_heights"][i],
+                                "prominence":properties["prominences"][i],
+                                "width":properties["widths"][i],
+                               }])
+        peaks_test.append({"properties":properties})
+        print(json.dumps(peaks_test, indent=2,cls=NumpyEncoder), file=open(f'{peak_dir}/{clip_name}_{index}_{section_ts}.txt', 'w'))
+
+
+    if len(peaks) != 1:
+        print(f"failed verification for {section_ts} due to multiple peaks {peaks} or zero peaks")
+        return False
+
+    index_final=0
+    #peak = peaks[index_final]
+    passed = properties["peak_heights"][index_final] == 1.0 and properties["prominences"][index_final] > 0.7 and properties["widths"][index_final] <= 10
+    if not passed:
+        print(f"failed verification for {section_ts} due to peak {peaks[index_final]} not meeting requirements")
+    return passed
+
 
 def smooth_preserve_peaks(data, window_size, threshold=0.1):
   """
