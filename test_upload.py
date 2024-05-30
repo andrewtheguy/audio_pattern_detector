4f00d610908c3412e519e74ab2d5603be7041b0a
Andrew Chen <andrew@jaanuu.com>
2024-05-30 07:56:44 -0700

    fix

--- test_upload.py
+++ test_upload.py
@@ -1,2 +1,3 @@
-from file_upload.upload_utils2 import upload_file
-print(upload_file('test1.py',"/test.txt",skip_if_exists=True))
+if __name__ == "__main__":
+    from file_upload.upload_utils2 import upload_file
+    print(upload_file('test1.py',"/test.txt",skip_if_exists=True))
