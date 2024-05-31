68f4c8b63353eae27220beb6d9ed98b1c88c8c16
Andrew Chen <andrew@jaanuu.com>
2024-05-30 23:08:47 -0700

    use rclone for file transfer

--- file_upload/upload_utils2.py
+++ file_upload/upload_utils2.py
@@ -1,86 +1,33 @@
-# stackoverflow's approach to create directories in sftp
-# https://stackoverflow.com/questions/14819681/upload-files-using-sftp-in-python-but-create-directories-if-path-doesnt-exist
-# def is_sftp_dir_exists(sftp, path):
-#     try:
-#         sftp.stat(path)
-#         return True
-#     except Exception:
-#         return False
-
-
-# def create_sftp_dir(sftp, path):
-#     try:
-#         sftp.mkdir(path)
-#     except IOError as exc:
-#         if not is_sftp_dir_exists(sftp, path):
-#             raise exc
-
-
-# def create_sftp_dir_recursive(sftp, path):
-#     parts = deque(Path(path).parts)
-
-#     to_create = Path()
-#     while parts:
-#         to_create /= parts.popleft()
-#         create_sftp_dir(sftp, str(to_create))
-
+import json
 import logging
-import os
-import paramiko
-from webdav4.client import Client
+import subprocess
 
 #from webdav4.client import Client
 logger = logging.getLogger(__name__)
 
-from upload_utils.sftp import create_remote_sftp_dir_recursively,file_exists
+rclone_config_file = "./rclone.conf"
+rclone_backend = "sftp"
 
-def connect_ssh_client(ssh_client):
-    # remote server credentials
-    host = "10.22.33.20"
-    username = "andrew"
-    password = "qwertasdfg"
-    port = '2022'
-    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh_client.connect(hostname=host,port=port,username=username,password=password, look_for_keys=False)
+# https://github.com/rclone/rclone/blob/master/bin/config.py
+# https://github.com/rclone/rclone/blob/2257c03391be526011743e2fe80fc7c6b1e23179/docs/content/docs.md for exit codes
+def stat(remote_path):
+    cmd = ["rclone", "--config", rclone_config_file, "lsjson", f"{rclone_backend}:{remote_path}"]
+    try:
+        result = subprocess.run(cmd, stdout=subprocess.PIPE,stderr=subprocess.PIPE, check=True)
+        return json.loads(result.stdout)
+    except subprocess.CalledProcessError as e:
+        # 3 - Directory not found, 4 - File not found
+        if e.returncode in [3, 4]:
+            return None
+        raise e
 
 def upload_file(file,dest_path,skip_if_exists=False):
-    # create ssh client 
-    with paramiko.SSHClient() as ssh_client:
-        connect_ssh_client(ssh_client)
-
-        # create an SFTP client object
-        with ssh_client.open_sftp() as sftp:
-            if skip_if_exists:
-                if file_exists(sftp_client=sftp, remote_path=dest_path):
-                    print(f'upload_file_with_sftp: file {dest_path} already exists,skipping')
-                    return
-            print(f"uploading {file} to {dest_path}")
-            create_remote_sftp_dir_recursively(sftp_client=sftp, remote_dir=os.path.dirname(dest_path))
-            sftp.put(file,dest_path)
-
-
-def upload_file_with_webdav(file,dest_path,skip_if_exists=False):
-    client = Client("http://10.22.33.20:9080", auth=("andrew", "qwertasdfg"))
-    
-    if(skip_if_exists and client.exists(dest_path)):
-        logger.info(f"webdav: file {dest_path} already exists,skipping")
+    if skip_if_exists and stat(dest_path):
+        print(f'upload_file: file {dest_path} already exists,skipping')
         return
+    cmd = ["rclone","-v", "--config", rclone_config_file, "copy", file, f"{rclone_backend}:{dest_path}"]
+    subprocess.run(cmd, check=True)
 
-    dir=os.path.dirname(dest_path)
-    if not client.exists(dir):
-        client.mkdir(dir)
-    logger.info(f"uploading {file} to {dest_path}")
-#    client.mkdir(dir)
-    client.upload_file(file,dest_path,overwrite=True)
-
-def sftp_file_exists(remote_path):
-    with paramiko.SSHClient() as ssh_client:
-        connect_ssh_client(ssh_client)
-
-        # create an SFTP client object
-        with ssh_client.open_sftp() as sftp:
-            #create_remote_sftp_dir_recursively(sftp_client=sftp, remote_dir="/chafa/trimmed/chilo")
-            return file_exists(sftp_client=sftp, remote_path=remote_path)
 
 if __name__ == '__main__':
-    upload_file("test_upload.py","/chilo/chjafa/test_upload.py")
+    upload_file("test data.txt","/chilo/chjafhtfhfga2/chafa1.txt", skip_if_exists=True)
