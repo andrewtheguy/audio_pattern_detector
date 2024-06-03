ab1023684150f7aa8384db96ee9a2b1cb50a36f7
Andrew Chen <andrew@jaanuu.com>
2024-06-03 11:43:38 -0700

    fix

--- schedule.py
+++ schedule.py
@@ -1,14 +1,14 @@
 import logging
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-from scrape_rthk import download_and_scrape
+from scrape_rthk import process_podcasts
 
 logging.basicConfig(level=logging.INFO)
 #logging.getLogger('scrape').setLevel(logging.INFO)
 logging.getLogger('apscheduler').setLevel(logging.INFO)
 if __name__ == '__main__':
     scheduler = BlockingScheduler()
-    scheduler.add_job(download_and_scrape, 'interval', hours=1)
+    scheduler.add_job(process_podcasts, 'interval', hours=1)
     # add a job to run immediately
-    scheduler.add_job(download_and_scrape)
+    scheduler.add_job(process_podcasts)
     scheduler.start()
