436423e49e2dc5ad44c72487a3077c6d60fb0ed9
Andrew Chen <andrew@jaanuu.com>
2024-06-11 21:52:15 -0700

    all

--- Dockerfile
+++ Dockerfile
@@ -41,4 +41,4 @@ COPY --from=rclone /usr/local/bin/rclone /usr/local/bin/rclone
 
 ENTRYPOINT ["/usr/bin/tini", "--"]
 
-CMD ["python", "schedule.py","rthk"]
+CMD ["python", "schedule.py","all"]
--- schedule.py
+++ schedule.py
@@ -8,6 +8,11 @@ import scrape_standard
 logging.basicConfig(level=logging.INFO)
 #logging.getLogger('scrape').setLevel(logging.INFO)
 logging.getLogger('apscheduler').setLevel(logging.INFO)
+
+def process_all_podcasts():
+    scrape_rthk.process_podcasts()
+    scrape_standard.process_podcasts()
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('podcast')
@@ -21,4 +26,10 @@ if __name__ == '__main__':
         scheduler.add_job(scrape_standard.process_podcasts, 'interval', hours=1)
         # add a job to run immediately
         scheduler.add_job(scrape_standard.process_podcasts)
+    elif args.podcast == 'all':
+        scheduler.add_job(process_all_podcasts, 'interval', hours=1)
+        # add a job to run immediately
+        scheduler.add_job(process_all_podcasts)
+    else:
+        raise ValueError('Invalid podcast name')
     scheduler.start()
