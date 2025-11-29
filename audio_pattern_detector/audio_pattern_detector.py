2c39e6f99637126c6b0ab26de6b4f9e4f8805ed9
Andrew Chen <andrew@jaanuu.com>
2025-11-28 21:27:42 -0800

    pyright

--- audio_pattern_detector/audio_pattern_detector.py
+++ audio_pattern_detector/audio_pattern_detector.py
@@ -131,7 +131,7 @@ class AudioPatternDetector:
 
             # Debug output for correlation
             if self.debug_mode:
-                import matplotlib.pyplot as plt
+                import matplotlib.pyplot as plt  # type: ignore[import-not-found]
                 print(f"clip_length {clip_name}", len(clip), file=sys.stderr)
                 print(f"clip_length {clip_name} seconds", len(clip) / self.target_sample_rate, file=sys.stderr)
                 print("correlation_clip_length", len(correlation_clip), file=sys.stderr)
@@ -262,7 +262,7 @@ class AudioPatternDetector:
             i = i + 1
 
         if self.debug_mode:
-            import matplotlib.pyplot as plt
+            import matplotlib.pyplot as plt  # type: ignore[import-not-found]
 
             suffix = full_audio_name
 
@@ -441,7 +441,7 @@ class AudioPatternDetector:
         section_ts = seconds_to_time(seconds=index * seconds_per_chunk, include_decimals=False)
 
         if debug_mode:
-            import matplotlib.pyplot as plt
+            import matplotlib.pyplot as plt  # type: ignore[import-not-found]
             print("---",file=sys.stderr)
             print(f"section_ts: {section_ts}, index {index}",file=sys.stderr)
             graph_dir = f"./tmp/graph/cross_correlation/{clip_name}"
@@ -605,7 +605,7 @@ class AudioPatternDetector:
         diff_overlap_ratio = area_prop["diff_overlap_ratio"]
 
         if debug_mode:
-            import matplotlib.pyplot as plt
+            import matplotlib.pyplot as plt  # type: ignore[import-not-found]
             print("similarity", similarity,file=sys.stderr)
             seconds.append(peak / sr)
             similarity_debug[clip_name].append((index, similarity,))
@@ -770,7 +770,7 @@ class AudioPatternDetector:
         similarity_whole = similarity
 
         if debug_mode:
-            import matplotlib.pyplot as plt
+            import matplotlib.pyplot as plt  # type: ignore[import-not-found]
             print("similarity", similarity,file=sys.stderr)
             seconds.append(peak / sr)
             similarity_debug[clip_name].append((index, similarity,))
